f4e86dedd28cbf389debce7727995e81df620afa
AetherZelta <isaacbravom10@gmail.com>
2024-03-04 15:50:38 -0600

    Uso de dio y consumo de api

---
+++ lib/network/api_popular.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:programovil/model/popular_model.dart';
+
+class ApiPopular {
+  final URL =
+      "https://api.themoviedb.org/3/movie/popular?api_key=73c295392359efe7df0a34433173fd7e&language=es-MX&page=1";
+  final dio = Dio();
+  Future<List<PopularModel>?> getPopularMovie() async {
+    Response response = await dio.get(URL);
+    if (response.statusCode == 200) {
+      final listMoviesMap = jsonDecode(response.data)['results'] as List;
+      return listMoviesMap.map((movie) => PopularModel.fromMap(movie)).toList();
+    }
+    return null;
+  }
+}
